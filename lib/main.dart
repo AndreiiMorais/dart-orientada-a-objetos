74c9aafe61ebc93e620a7a7832a104535d2ad861
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-13 14:38:39 -0300

    criada classe pessoa

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'package:dart_oo/src/pessoa.dart';
+
 void main(List<String> arguments) {
-  
+  Pessoa pessoa = Pessoa("Andrei", 28, "M");
+  print(pessoa.nome);
+  print(pessoa.idade);
+  print(pessoa.sexo);
 }
---
+++ lib/src/pessoa.dart
@@ -0,0 +1,8 @@
+class Pessoa {
+  String nome = "";
+  int idade = 0;
+  String sexo = "";
+
+  Pessoa(this.nome, this.idade, this.sexo);
+  //para criar um construtor nao precisa ter corpo {} e nem especificar o tipo da variavel, somente usar o this.
+}
