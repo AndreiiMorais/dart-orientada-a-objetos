2e87b3c933595e8a1d1d49ca35d493b889eed04d
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-13 14:52:56 -0300

    modificacao no construtor

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:dart_oo/src/pessoa.dart';
 
 void main(List<String> arguments) {
-  Pessoa pessoa = Pessoa("Andrei", 28, "M");
+  Pessoa pessoa = Pessoa(idade: 28, nome: "Andrei", sexo: "M");
+  //dessa forma que esta instanciada, nao necessita estar na mesma ordem da classe
+  
   print(pessoa.nome);
   print(pessoa.idade);
   print(pessoa.sexo);
--- lib/src/pessoa.dart
+++ lib/src/pessoa.dart
@@ -3,6 +3,8 @@ class Pessoa {
   int idade = 0;
   String sexo = "";
 
-  Pessoa(this.nome, this.idade, this.sexo);
+  Pessoa({required this.nome, required this.idade, required this.sexo});
+  // o required é pra nao deixar Null no valor
+  //utilizando as {} dentro do () libera na hora de instanciar para nao ser necessaria a ordem exata.
   //para criar um construtor nao precisa ter corpo {} e nem especificar o tipo da variavel, somente usar o this.
 }
