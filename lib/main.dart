f660767bc2dcc0eaa6a203dd69872814024abcb5
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-13 15:10:02 -0300

    adicionada Heranca
    pessoa herdou caracteristicas de Humano

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:dart_oo/src/pessoa.dart';
 void main(List<String> arguments) {
   Pessoa pessoa = Pessoa(idade: 28, nome: "Andrei", sexo: "M");
   //dessa forma que esta instanciada, nao necessita estar na mesma ordem da classe
-  
+
   print(pessoa.nome);
   print(pessoa.idade);
   print(pessoa.sexo);
+  //altura e peso sao metodos herdados da classe humano para a classe pessoa.
+  print(pessoa.altura);
+  print(pessoa.peso);
 }
--- lib/src/pessoa.dart
+++ lib/src/pessoa.dart
@@ -1,8 +1,15 @@
-class Pessoa {
+import 'package:dart_oo/src/humano.dart';
+
+class Pessoa extends Humano {
+  //extends == herança..
   String nome = "";
   int idade = 0;
   String sexo = "";
+  //para criar uma variavel local privada, apenas precisa colocar _ antes do nome ex: String _sexo;
 
+  //variaveis do tipo final String sexo; so podem ser instanciadas uma unica vez.
+
+  //Construtor
   Pessoa({required this.nome, required this.idade, required this.sexo});
   // o required é pra nao deixar Null no valor
   //utilizando as {} dentro do () libera na hora de instanciar para nao ser necessaria a ordem exata.
